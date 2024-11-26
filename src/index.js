f4481529e9b4aea40de842a756c81faeacc26442
Ralatcode <ralatrado@gmail.com>
2024-11-26 20:11:42 +0100

    made home page a module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('js works')
+// import "./style.css";
+import { homeH1, homeImg, homePara } from "./home";
+
+const contentDiv = document.querySelector('#content');
+contentDiv.append(homeImg, homeH1, homePara);

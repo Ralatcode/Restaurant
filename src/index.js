2e9949e35108817ae150aac375ed7e7d7e3220b3
Ralatcode <ralatrado@gmail.com>
2024-11-27 12:45:05 +0100

    page tab logic

--- src/index.js
+++ src/index.js
@@ -2,4 +2,27 @@ import "./style.css";
 import { homeH1, homeImg, homePara } from "./home";
 
 const contentDiv = document.querySelector('#content');
-contentDiv.append(homeImg, homeH1, homePara);
+const navBtns = Array.from(document.querySelectorAll("nav > button"));
+let currentPage = 'home';
+
+switchPage(currentPage);
+
+navBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        currentPage = e.target.id;
+        switchPage(currentPage);
+    })
+})
+
+function switchPage(page) {
+    if (page === 'home') {
+        contentDiv.textContent = '';
+        contentDiv.append(homeImg, homeH1, homePara);   
+    } else if (page === 'menu') {
+        contentDiv.textContent = '';
+        console.log('display menu page');
+    } else if (page === 'about') {
+        contentDiv.textContent = '';
+        console.log('display about page');
+    }
+}

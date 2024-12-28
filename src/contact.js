6249e1e06fbba8f6d11d018ff6446fac2065dd65
Ralatcode <ralatrado@gmail.com>
2024-12-28 15:07:38 +0100

    content loader function

--- src/contact.js
+++ src/contact.js
@@ -3,6 +3,9 @@ const contactImg = document.createElement('img');
 const contactPara = document.createElement('p');
 const heroDiv = document.createElement('div');
 const contactContent = document.createElement('div');
+const aboutDiv = document.createElement('div');
+const contactDiv = document.createElement('div');
+
 
 contactImg.src = 'https://www.touchpoint.com/wp-content/uploads/2023/10/Featured-image-4.png';
 contactImg.alt = 'contact';
@@ -10,6 +13,16 @@ contactImg.classList.add('home-img');
 contactPara.textContent = ' Join us and savor the essence of Nigeria.';
 contactH1.textContent = "Ralat Restaurant";
 heroDiv.append(contactImg, contactH1, contactPara);
+
+function loadAboutContent(parent) {
+    const aboutH3 = document.createElement('h3');
+    const aboutP1 = document.createElement('p');
+    const aboutP2 = document.createElement('p');
+}
+function loadContactContent(parent) {
+    const contactH3 = document.createElement('h3');
+}
+
 
 contactContent.append(heroDiv);
 
